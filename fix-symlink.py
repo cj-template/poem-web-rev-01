1f62b4476b537853de715235a8263ab5eeee2013
Chris Jackson <hello@cjjackson.dev>
2025-10-15 12:50:57 +0100

    added: fix-symlink.py

---
+++ fix-symlink.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+current_dir = os.getcwd()
+
+os.chdir("backoffice/asset/embed_hidden/js")
+os.symlink("../../embed/js", "assets", target_is_directory=True)
+
+os.chdir(current_dir)
